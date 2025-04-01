20cc30d55fc7927ad19179b501b5ec11403977ba
benjaminfrias <benjaminfrias2004@gmail.com>
2025-04-01 14:27:13 -0400

    feat: add root page

---
+++ src/routes/Root.jsx
@@ -0,0 +1,14 @@
+function Root() {
+    return (
+        <div className="container">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Shop</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Root;
